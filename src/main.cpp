486dba3ae98f683c6dc5ceb74bd559366e3aaf82
vladimir <vladimir.oleynyk057@gmail.com>
2025-10-02 20:55:18 +0300

    applied some changes

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,33 @@ int main() {
 
         std::cout << "Matrix A: " << "\n";
         print_matrix(A);
+        std::cout << '\n';
 
         std::cout << "Matrix B: " << "\n";
         print_matrix(B);
+        std::cout << '\n';
 
         std::cout << "A + B: " << "\n";
         Matrix Res = matrix_add(A, B);
         print_matrix(Res);
+        std::cout << '\n';
+
+        std::cout << "A * B: " << '\n';
+        Matrix res1 = matrix_multiply(A, B);
+        print_matrix(res1);
+        std::cout << '\n';
+
+
+        std::cout << "transponse matrix:" << '\n';
+        Matrix res2 = matrix_transpose(A);
+        print_matrix(res2);
+        std::cout << '\n';
 
         free_matrix(A);
         free_matrix(B);
         free_matrix(Res);
+        free_matrix(res1);
+        free_matrix(res2);
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << "\n";
         return 1;
